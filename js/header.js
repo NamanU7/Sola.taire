2446be42938dfa8f61e8bc01ff669c07784e17e0
Naman Uttamchandani <naman.uttam7@gmail.com>
2022-02-28 14:08:58 -0800

    Design of roadmap complete, not dynamic yet.

--- js/header.js
+++ js/header.js
@@ -1,29 +1,3 @@
-const header = document.getElementById('navbar');
 
-header.innerHTML = `
-<div class="container">
-<a href="#" class="logo">Sola.<span class="logo-taire">taire</span>
-</a>
-
-<img id="mobile-cta" class="mobile-menu" src="assets/menu.svg" alt="Open Navigation">
-
-<nav>
-    <img id="mobile-exit" class="mobile-menu-exit" src="assets/close.svg" alt="Exit Navigation">
-
-    <ul class="primary-nav">
-        <li class="current"><a href="index.html">Home</a></li>
-        <li><a href="faqs.html">FAQs</a></li>
-        <li><a href="roadmap.html">Roadmap</a></li>
-    </ul>
-
-
-    <ul class="secondary-nav">
-        <li><a href="#">Community</a></li>
-        <li class="mint-cta"><a href="#">Mint</a></li>
-    </ul>
-
-</nav>
-</div>
-`;
 
 
